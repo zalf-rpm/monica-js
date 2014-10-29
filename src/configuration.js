8a9eff368814e43e3a3ada58814070f1f8cbbc05
jvail <jv@zalf.de>
2014-10-29 12:10:24 +0100

    Fix output date format

--- src/configuration.js
+++ src/configuration.js
@@ -658,7 +658,7 @@ var Configuration = function (outPath, climate, doDebug) {
         ;
 
       progress = {
-          date: { value: date.toLocaleDateString(), unit: '[date]' }
+          date: { value: date.toISOString(), unit: '[date]' }
         , CropName: { value: isCropPlanted ? mcg.get_CropName() : '', unit: '-' }
         , TranspirationDeficit: { value: isCropPlanted ? mcg.get_TranspirationDeficit() : 0, unit: '[0;1]' }
         , ActualTranspiration: { value: isCropPlanted ? mcg.get_ActualTranspiration() : 0, unit: '[mm]' } 
